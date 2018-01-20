af4b39ba500137d7538f81ee29fea7bd0c1728ff
rootwzrd <rootwzrd@gmail.com>
2018-01-20 05:52:53 -0500

    initial commit with smst file

---
+++ smst.php
@@ -0,0 +1,74 @@
+i<?php 
+ 
+// Get the PHP helper library from twilio.com/docs/php/install 
+// require_once '../vmail/lib/Twilio/autoload.php'; // Loads the library 
+// //include("functions.php");
+//
+// use Twilio;
+// $response = new Twilio\Twiml();
+// //date_default_timezone_set('UTC');
+// //$fromNumber = '+12345678990';
+// //$account_sid = "ACCOUNT_SID"; 
+// //$auth_token = "AUTH_TOKEN"; 
+//
+// //nmessage = 'Primis facilis apeirian vis ne. Idque ignota est ei. Ut sonet indoctum nam, ius ea illum fabellas. Pri delicata percipitur ad, munere ponderum rationibus vix et. Zzril vocent salutatus ut vix, mei recteque periculis no, ea vel aeterno lobortis. In vis insolens patrioque voluptatum. Hinc tibique vix ei, appetere probatus pro an. Dicta nusquam scriptorem vix ut, eu suas conclusionemque cum. Eros cibo equidem cu vel. Fabellas atomorum partiendo sea ad, hinc malorum usu in. Vocibus constituam eum ei. Has in simul quodsi similique, vis eu autem novum omittam. An meis tempor nam, scripta legimus incorrupte no mea. In nam graece commune. Numquam debitis aliquyam eos ex, latine veritus ad pro. Et consul mollis prodesset sit, sea graeco periculis dissentias te. Per te enim inermis definitiones. His ad vivendum platonem reformidans. Mentitum postulant persequeris has et, graece primis in eos. Mei in rebum nominavi eleifend, duo evertitur definitionem et. Magna interesset at mei, audire repudiandae voluptatibus sit ex. Ex his clita patrioque disputando, ei illud tantas quo. Nec dicam melius invidunt no, placerat salutandi ut eum, facete patrioque consectetuer has eu.';
+//
+//
+//
+//
+// //hunks = explode("||||",wordwrap($inmessage,155,"||||"));
+// //otal = count($chunks);
+// //ho $chunks;
+//
+//
+//
+//
+//
+// $messageq = 'Primis facilis apeirian vis ne. Idque ignota est ei. Ut sonet indoctum nam, ius ea illum fabellas. Pri delicata percipitur ad, munere ponderum rationibus vix et. Zzril vocent salutatus ut vix, mei recteque periculis no, ea vel aeterno lobortis. In vis insolens patrioque voluptatum. Hinc tibique vix ei, appetere probatus pro an. Dicta nusquam scriptorem vix ut, eu suas conclusionemque cum. Eros cibo equidem cu vel. Fabellas atomorum partiendo sea ad, hinc malorum usu in. Vocibus constituam eum ei. Has in simul quodsi similique, vis eu autem novum omittam. An meis tempor nam, scripta legimus incorrupte no mea. In nam graece commune. Numquam debitis aliquyam eos ex, latine veritus ad pro. Et consul mollis prodesset sit, sea graeco periculis dissentias te. Per te enim inermis definitiones. His ad vivendum platonem reformidans. Mentitum postulant persequeris has et, graece primis in eos. Mei in rebum nominavi eleifend, duo evertitur definitionem et. Magna interesset at mei, audire repudiandae voluptatibus sit ex. Ex his clita patrioque disputando, ei illud tantas quo. Nec dicam melius invidunt no, placerat salutandi ut eum, facete patrioque consectetuer has eu.';
+//
+// $chunks = explode("||||",wordwrap($messageq,155,"||||"));
+// $total = count($chunks);
+//
+//
+// //$response=>message("                    <?xml version="1.0" encoding="UTF-8"? >
+//
+// echo "<" . "?xml version =" . '"' . "1.0" . '"' ;
+// echo " encoding =" . '"' . "UTF-8" . '"' . "?" . ">" ;
+// echo "<Response>" . "<Message to=" . "'" . "sim:DEd6ec0771ab3b58d113902237c2d29d4f" . "' " . "from=" . "'" . "+17028197420" . "' > " ;
+//
+// foreach($chunks as $page => $chunk)
+// {
+//   //  echo sprintf("(%d/%d) ",$page+1,$total);
+//     $page+1;
+//       $total;  
+//          echo " <Body>" . "$chunk" . "</Body> " ;
+//             
+//             }
+//             echo "</Message> " . "</Response>" ;
+//             //echo $boDy;
+//
+//
+//
+//
+//
+//
+//
+//             //", array("to" => 'sim:DEd6ec0771ab3b58d113902237c2d29d4f', "from" => '+17028197429' ));
+//             //echo $response ;
+//
+//             //oreach($chunks as $page => $chunk)
+//                       //        {
+//                        
+//                                           
+//                                            //ho $chunks[1];
+//                                               //              $mesgNum = sprintf("(%d/%d) ",$page+1,$total);
+//                                                    //            $bodymsg = "$chunk" ;
+// //$msgfrag = $mesgNum . " " . $bodymsg ;
+// //$body[$page] = $msgfrag;
+// //           $sect = $page;
+// //message = $response->message( " $bodymsg " , array("to" => 'sim:DEd6ec0771ab3b58d113902237c2d29d4f', "from" => '+16039153666' ));
+///echo $response;
+////
+//
+//?>
+//
